5aecb274381f3fee255a0e40c97037c45851a5de
somesh202 <77766761+somesh202@users.noreply.github.com>
2021-07-28 23:06:22 +0530

    validated URL using dns

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const validUrl = require('valid-url');
-const shortID = require('short-id');
-const URL = require('./models/url')
-var BodyParser = require('body-parser')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const shortID = require("shortid");
+const URL = require("./models/url");
+var BodyParser = require("body-parser");
+const dns = require("dns");
+const urlparser = require("url");
 const app = express();
 
 
 
-
-
-
-// parse application/json
-
 const connectDB = require('./configs/db');
-// Basic Configuration
+
 const port = process.env.PORT || 3000;
 
 connectDB();
@@ -30,7 +27,6 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
@@ -42,50 +38,52 @@ app.listen(port, function () {
 app.post("/api/shorturl", async (req, res) => {
   const url = req.body['url'];
   const urlCode = shortID.generate();
+  const checkValidUrl = dns.lookup(urlparser.parse(url).hostname, (err, address) => {
 
-  
-  if(!validUrl.isUri(url)) {
-    res.status(400).json({
-      error: "Invalid URL"
-    });
-  }
-  else{
-       await URL.findOne({original_url: url}, (err, rec) => {
-         if(rec) {
-           console.log("Record found in DB");
-           res.json({
-             original_url: url,
-             short_url: urlCode
-           })
-         }
-         else{
-           console.log("record not found in DB, creating a new one..");
-           let newUrl = new URL({
-             original_url: url,
-             short_url: urlCode
-           });
-           newUrl.save((err) => {
-             if(err) return console.log(err);
-             res.json({
-               original_url: url,
-               short_url: urlCode
-             })
-           })
-         }
-       })
+    if (!address) {
+      res.json({
+        error: "invalid url"
+      });
+    }
 
- 
-  }
-})
+    else {
+      URL.findOne({ original_url: url }, (err, rec) => {
+        if (rec) {
+          console.log("Record found in DB");
+          res.json({
+            original_url: url,
+            short_url: urlCode
+          })
+        }
+        else {
+          console.log("record not found in DB, creating a new one..");
+          let newUrl = new URL({
+            original_url: url,
+            short_url: urlCode
+          });
+          newUrl.save((err) => {
+            if (err) return console.log(err);
+            res.json({
+              original_url: url,
+              short_url: urlCode
+            })
+          })
+        }
+      })
+    }
+  })
+});
 
 app.get("/api/shorturl/:short_url", async (req, res) => {
-  try{
+  try {
     const doc = await URL.findOne({
       short_url: req.params.short_url
-  })
-  res.redirect(doc.original_url)
+    })
+    res.redirect(doc.original_url)
   }
-catch(err) {
-  res.status(500).send(err);
-}
+  catch (err) {
+    res.json({
+      error: 'invalid url'
+    })
+  }
 })
